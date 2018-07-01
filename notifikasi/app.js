d8a77ba1d11211234e8989d86548d3acda7646cd
Rafi Priatna K <root@rafipriatna.web.id>
2018-07-02 00:02:36 +0700

    Update app.js

--- notifikasi/app.js
+++ notifikasi/app.js
@@ -1,4 +1,6 @@
-  
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
+
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyB5RVi1468NVOkNcD9r7FmEaGOjkJscUtQ",
