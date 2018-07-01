0fcbed73c4212ce4fe2e4bb9098eaa0086ec0057
Rafi Priatna K <root@rafipriatna.web.id>
2018-07-01 21:47:41 +0700

    Create app.js

---
+++ notifikasi/app.js
@@ -0,0 +1,27 @@
+  
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB5RVi1468NVOkNcD9r7FmEaGOjkJscUtQ",
+    authDomain: "rafi-priatna-notifikasi.firebaseapp.com",
+    databaseURL: "https://rafi-priatna-notifikasi.firebaseio.com",
+    projectId: "rafi-priatna-notifikasi",
+    storageBucket: "rafi-priatna-notifikasi.appspot.com",
+    messagingSenderId: "447298606224"
+  };
+  firebase.initializeApp(config);
+  
+  const messaging = firebase.messaging();
+  messaging.requestPermission().then(function() {
+     //getToken(messaging);
+     return messaging.getToken();
+  }).then(function(token){
+  console.log(token);
+  })
+.catch(function(err) {
+  console.log('Permission denied', err);
+});
+
+
+messaging.onMessage(function(payload){
+console.log('onMessage: ',payload);
+});
