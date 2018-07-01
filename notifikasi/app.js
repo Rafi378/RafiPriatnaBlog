3b5d30130d2e3bb377d2f1d7e91fa3c10dfb2e73
Rafi Priatna K <root@rafipriatna.web.id>
2018-07-02 00:20:43 +0700

    Update app.js

--- notifikasi/app.js
+++ notifikasi/app.js
@@ -11,16 +11,10 @@
   
   const messaging = firebase.messaging();
   messaging.requestPermission().then(function() {
-     //getToken(messaging);
-     return messaging.getToken();
-  }).then(function(token){
-  console.log(token);
-  })
-.catch(function(err) {
-  console.log('Permission denied', err);
-});
+ .then(function(){
+   console.log('Ada izin');
+ })
 
-
-messaging.onMessage(function(payload){
-console.log('onMessage: ',payload);
-});
+.catch.(function(err){
+   console.log('Error gan');
+ })
